35f666a2b7b39ea7e240e6171d59afe991b2dbf0
liamthetaylor <141086754+liamthetaylor@users.noreply.github.com>
2023-11-07 12:28:12 +1300

    Add files via upload

--- Bits_calculator.py
+++ Bits_calculator.py
@@ -9,9 +9,8 @@ def statement_generator(text, decoration):
     print()
     return ""
 
-# Checks user choice is text, integer or image
 
-
+    # Checks user choice is text, integer or image
 def user_choice():
     text_ok = ["text", "t", "txt"]
     image_ok = ["im", "image", "pic", "picture", "photo"]
@@ -36,7 +35,7 @@ def user_choice():
             print("Please choose valid file type.")
             print()
 
-
+    # Checks integer fits in parameters
 def integer_checker(question, low):
     valid = False
     while not valid:
@@ -58,9 +57,8 @@ def integer_checker(question, low):
     print("The integer you have chosen is", response)
     print()
 
-
+    # Coverts text to bits
 def text_bits():
-    print()
     var_text = input("Please enter some text: ")
     var_length = len(var_text)
     num_bits = 8 * var_length
@@ -72,6 +70,8 @@ def text_bits():
     return ""
 
 
+
+    # Converts image height and width into bits in image
 def image_bits():
     image_height = integer_checker("Image height: ", 1)
     image_width = integer_checker("Image width: ", 1)
@@ -83,7 +83,7 @@ def image_bits():
     print()
     return ""
 
-
+    # Converts integers to bits
 def integer_bits():
     var_int = integer_checker("Please enter an integer: ", 0)
     var_bin = "{0:b}".format(var_int)
@@ -97,32 +97,33 @@ def integer_bits():
 
 # main routine goes here
 
-# heading
-
-
+    # heading
 statement_generator("Bit calculator for text, integers and images", "~")
 
-# display instructions if it is users first time
+    # display instructions if it is users first time
 
-# loop to allow multiple calculations
+    # loop to allow multiple calculations
 keep_going = ""
 while keep_going == "":
 
-    # Ask user for file type
+        # Ask user for file type
     data_type = user_choice()
     print()
     print("You chose", data_type)
     print()
 
-    # For integers, ask user for integer
-    # (must be integer more / equal to 0)
+        # For integers, ask user for integer
+        # (must be integer more / equal to 0)
     if data_type == "integer":
         integer_bits()
 
-    # For images, ask user for width and height
-    # (must be integer more / equal to 1)
+        # For images, ask user for width and height
+        # (must be integer more / equal to 1)
     elif data_type == "image":
         image_bits()
 
     elif data_type == "text":
         text_bits()
+
+    keep_going = input("Press <enter> to continue and any key to quit: ")
+    print()
